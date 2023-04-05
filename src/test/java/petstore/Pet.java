dfa849f14f6866b2cf8b1626aae4ceb98c3df186
Vinicius Brussulo Cabrera Alves da Silva <vbrussulo@foursys.com.br>
2023-04-04 23:20:59 -0300

    Criação do teste para icluir um pet

---
+++ src/test/java/petstore/Pet.java
@@ -0,0 +1,42 @@
+
+package petstore;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+public class Pet {
+
+    // Atributos:
+    String uri = "https://petstore.swagger.io/v2/pet"; // endereço da entidade exibida no swagger
+
+
+// Metodos e funções:
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+@Test
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet1.json"); // passando o caminho onde está o Body de envio
+
+        given()
+                .contentType("application/json")
+                .log().all() // mostrar o retorno no console
+                .body(jsonBody)
+
+        .when()
+                .post(uri)
+
+        .then()
+                .log().all() // mostrar o retorno no console
+                .statusCode(200)
+        ;
+
+     }
+
+}
